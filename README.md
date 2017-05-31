ca581bfed67aeb5d7077be09e23453b53b059f2a
Blayne Marjama <blayne.marjama@gmail.com>
2017-05-31 15:54:11 -0700

    uh yeah no

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 # Form-Builder
 
 Will create forms depending on what fields are chosen. Will then generate a way to copy and paste the component into a persons code.
+
+### Disclaimer
+
+I don't think the technology is here yet.. But we can try.
--- src/components/well_code.js
+++ src/components/well_code.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import reactElementToJSXString from 'react-element-to-jsx-string';
 import { Well, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
+let mhm
 class WellCode extends Component {
+	constructor(props){
+		super(props)
+
+		this.renderGroups = this.renderGroups.bind(this)
+		this.createMarkup = this.createMarkup.bind(this)
+	}
 	renderGroups(){
-		return this.props.formObjects.map((item, i) =>{
+		return mhm = this.props.formObjects.map((item, i) =>{
 			return (
 				<Col key={item + i} xs={6}>
 					<h4>Id:{i}</h4>
@@ -15,9 +23,12 @@ class WellCode extends Component {
 			)
 		})
 	}
-				// <pre><code>
-				// </code></pre>
+
+	createMarkup() {
+	  return { __html: this.renderGroups()};
+	}
 	render() {
+		console.log(this.props)
 		const h4="<h4>", h4c="</h4>", p="<p>", pc="</p>"
 		const uh = `
 class WellCode extends Component {
@@ -32,7 +43,7 @@ class WellCode extends Component {
 		return (
 			<Well className="row">
 				<pre>{uh}</pre>
-				{this.renderGroups()}
+				<div dangerouslySetInnerHTML={this.createMarkup()} />
 			</Well>
 		)
 	}
@@ -45,5 +56,4 @@ function mapStateToProps(state){
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ updateFormName, updateFormType, updateFormData }, dispatch)
 // }
-
 export default connect(mapStateToProps, {})(WellCode);
