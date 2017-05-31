0a5fb1873cf43304c4bea82c9a10d79cda4a914e
Blayne Marjama <blayne.marjama@gmail.com>
2017-05-30 22:40:00 -0700

    trying to figure out pre/code

--- src/components/well_code.js
+++ src/components/well_code.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import { Well } from 'react-bootstrap';
+import { Well, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-
+// import { bindActionCreators } from 'redux';
 class WellCode extends Component {
 	renderGroups(){
 		return this.props.formObjects.map((item, i) =>{
-			return <div key={item + i}>{JSON.stringify(item)}</div>
+			return (
+				<Col key={item + i} xs={6}>
+					<h4>Id:{i}</h4>
+					<p>fieldType: {item.fieldType}</p>
+					<p>fieldName: {item.fieldName}</p>
+					<p>fieldData: {item.fieldData}</p>
+				</Col>
+			)
 		})
 	}
-
+				// <pre><code>
+				// </code></pre>
 	render() {
+		const h4="<h4>", h4c="</h4>", p="<p>", pc="</p>"
+		const uh = `
+class WellCode extends Component {
+  render() {
+    return (
+      <Well className="row">
+        ${this.renderGroups()}
+      </Well>
+    )
+  }
+}`
 		return (
-			<Well>
+			<Well className="row">
+				<pre>{uh}</pre>
 				{this.renderGroups()}
 			</Well>
 		)
